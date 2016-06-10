406fdd7b02a80052e60d3f9f4c284211b1fdf5b0
Regis <boudinot.regis@yahoo.com>
2016-06-09 22:22:48 -0600

    super refactor

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,9 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 
 pub fn turn_into_pointer(st: String)  -> CString {
-    let concated_string = CString::new(st).unwrap();
-    unsafe { CString::from_raw(concated_string.into_raw()) }
+    unsafe { CString::from_raw(CString::new(st).unwrap().into_raw()) }
 }
 
 pub fn make_string(s1: *const c_char) -> String {
-    let cstr          = unsafe { CStr::from_ptr(s1) }; // &std::ffi::c_str::CStr
-    let string        = cstr.to_str().unwrap();        // &str
-    let return_string = string.to_string();            // collections::string::String
-    return_string
+    unsafe { CStr::from_ptr(s1) }.to_str().unwrap().to_string()
 }
